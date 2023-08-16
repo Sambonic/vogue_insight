1eaca87dcf5ab793916759b6b962d0ab48c54a81
Sambonic <124459402+Sambonic@users.noreply.github.com>
2023-08-16 18:57:05 +0300

    Initial Commit

---
+++ fasion_multi_class_classification.py
@@ -0,0 +1,99 @@
+# %%
+import tensorflow as tf
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+# Load and preprocess the Fashion MNIST dataset composed of 28x28 images
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+
+# Choose a subset size
+subset_size = 1000 
+train_images = train_images[:subset_size]
+train_labels = train_labels[:subset_size]
+
+# Print some of the data 
+train_df = pd.DataFrame({'Image': train_images.tolist(), 'Label': train_labels.tolist()})
+
+print("Train Data:")
+print(train_df.head())
+
+
+# Build the model
+model = Sequential([
+    Flatten(input_shape=(28, 28)),  
+    Dense(1000, activation='relu'),
+    Dense(10, activation='softmax')  
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+# Train, Evaluate and make predictions on the model
+model.fit(train_images, train_labels, epochs=10, validation_split=0.6)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
+
+predictions = model.predict(test_images)
+
+
+# Plot the image in gray scale
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel(f"{class_names[predicted_label]} {100*np.max(predictions_array):2.0f}% ({class_names[true_label]})", color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+# Test the trained model
+class_names = [
+    'T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+    'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'
+]
+
+num_rows = 2
+num_cols = 5
+num_images = num_rows * num_cols
+plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+    plot_image(i, predictions, test_labels, test_images)
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+    plot_value_array(i, predictions, test_labels)
+plt.show()
+
+
+
+
+
